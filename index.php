d68e7f624c4bd2f0849f209b94b84e2b242f21db
GOUTHAM1505 <70936094+GOUTHAM1505@users.noreply.github.com>
2022-05-12 20:22:40 +0530

    search

--- index.php
+++ index.php
@@ -1,51 +1,5 @@
-<?php
+<?php include('includes/header.php');?>
 
-include("functions/functions.php");
-include("includes/db.php");
-?>
-
-<html>
-
-<head>
-<title>FLIPZON</title>
-
-<link rel="stylesheet" href="styles/style1.css" media="all" />
-</head>
-
-<body>
-
-<!-- Main container starts here -->
-<div class="main_wrapper">
-
-	<div class="header_wrapper">
-	<div class="header_logo">
-	<a href="index.php">
-	<!-- <img id="logo" src="images/ample.png" height=30> -->
-
-	</a>
-	</div><!--/.header_logo-->
-
-	<div id="form">
-		<form method="get" action="results.php" enctype="multipart/form-data">
-		<input type="text" name="user_query" placeholder="Search a Product" />
-		<input type="submit" name="search" value="Search" />
-		</form>
-	</div>
-	<div class="cart">
-		<ul>
-			<li class="dropdown_header_cart">
-				<div id="notification_total_art" class="shopping-cart">
-					<img src="images/cart_icon.png" id="cart_image" height="50px" >
-				</div>
-			</li>	
-		</ul>
-	</div>
-	<div class="register_login">
-		<div class="login"><a href="index.php?action=login">Login</a></div>
-		<div class="register"><a href="customer_register.php">Register</a></div>
-	</div>
-	
-	</div><!--/.header_wrapper-->
 	<div class="menubar">
 		<ul id="menu">
 			<li><a href="index.php">Home</a></li>
@@ -89,13 +43,4 @@ include("includes/db.php");
 	</div>
 
 	</div><!-- /.content_wrapper-->
-
-	<div id="footer">
-	<h2 style="text-align:center; padding-top:30px">&copy; 2020 --<?php echo date('Y');?> by Baindla Goutham</h2>
-	</div>	
-
-</div><!-- /.main_wrapper-->
-<!-- End Main container starts here --->
-</body>
-
-</html>
+<?php include('includes/footer.php');?>
